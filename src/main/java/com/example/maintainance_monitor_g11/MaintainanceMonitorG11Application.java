2ebc20ac97e3a9a961147f420d04134eb046c67b
Christoph Kormesser <christoph.kormesser@gmail.com>
2022-11-20 14:12:13 +0100

    Initial commit

---
+++ src/main/java/com/example/maintainance_monitor_g11/MaintainanceMonitorG11Application.java
@@ -0,0 +1,13 @@
+package com.example.maintainance_monitor_g11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintainanceMonitorG11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintainanceMonitorG11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maintainance_monitor_g11/MaintainanceMonitorG11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maintainance_monitor_g11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintainanceMonitorG11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
