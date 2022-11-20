dad58ddbfc40dfc2da7f39427e5720d8d522dce5
Christoph Kormesser <christoph.kormesser@gmail.com>
2022-11-20 14:55:58 +0100

    initialized dev branch

--- src/test/java/com/example/maintainance_monitor_g11/MaintainanceMonitorG11ApplicationTests.java
+++ src/test/java/com/example/maintainance_monitor_g11/MaintainanceMonitorG11ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MaintainanceMonitorG11ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
